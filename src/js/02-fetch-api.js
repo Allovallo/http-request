9d6084c6d519782b3a430eaa42434479f7934e3d
Allovallo <30566134+Allovallo@users.noreply.github.com>
2023-12-11 11:22:46 +0200

    Files updated

--- src/js/02-fetch-api.js
+++ src/js/02-fetch-api.js
@@ -1,3 +1,14 @@
-fetch('https://pokeapi.co/api/v2/pokemon/2').then(data => {
-  console.log(data);
-});
+import '../css/common.css';
+
+import pokemonCardTpl from '../templates/pokemon-card.hbs';
+
+fetch('https://pokeapi.co/api/v2/pokemon')
+  .then(response => {
+    return response.json();
+  })
+  .then(pokemon => {
+    console.log(pokemon);
+    const markup = pokemonCardTpl(pokemon);
+    console.log(markup);
+  })
+  .catch(error => console.log(error));
