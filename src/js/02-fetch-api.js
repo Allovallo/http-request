635b484e9b33775177913ea1b4e72ddac7bb9200
Allovallo <30566134+Allovallo@users.noreply.github.com>
2023-12-14 14:23:56 +0200

    Files updated

--- src/js/02-fetch-api.js
+++ src/js/02-fetch-api.js
@@ -1,10 +1,9 @@
 import '../css/common.css';
 import pokemonCardTpl from '../templates/pokemon-card.hbs';
+import API from './api-service';
+import getRefs from './get-refs';
 
-const refs = {
-  cardContainer: document.querySelector('.js-card-container'),
-  searchForm: document.querySelector('.js-search-form'),
-};
+const refs = getRefs();
 
 refs.searchForm.addEventListener('submit', onSearch);
 
@@ -14,18 +13,23 @@ function onSearch(e) {
   const form = e.currentTarget;
   const searchQuery = form.elements.query.value;
 
-  fetchPokemon(searchQuery)
+  API.fetchPokemon(searchQuery)
     .then(renderPokemonCard)
     .catch(onFetchError)
     .finally(() => form.reset());
 }
 
-function fetchPokemon(pokemonId) {
-  const url = `https://pokeapi.co/api/v2/pokemon/${pokemonId}`;
-
-  return fetch(url).then(response => response.json());
+function renderPokemonCard(pokemon) {
+  const markup = pokemonCardTpl(pokemon);
+  refs.cardContainer.innerHTML = markup;
 }
 
 function onFetchError(error) {
   alert('Щось пійшло не так, ми не знайшли вашого покемона(');
 }
+
+// ======================================
+
+fetch('https://pokeapi.co/api/v2/pokemon?limit=10')
+  .then(r => r.json)
+  .then(console.log);
--- src/js/api-service.js
+++ src/js/api-service.js
@@ -1,4 +1,9 @@
-export function renderPokemonCard(pokemon) {
-  const markup = pokemonCardTpl(pokemon);
-  refs.cardContainer.innerHTML = markup;
+const BASE_URL = `https://pokeapi.co/api/v2`;
+
+function fetchPokemon(pokemonId) {
+  return fetch(`${BASE_URL}/pokemon/${pokemonId}`).then(response =>
+    response.json(),
+  );
 }
+
+export default { fetchPokemon };
